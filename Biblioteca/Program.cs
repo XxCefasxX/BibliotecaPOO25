7439abda3af65c69286a294e910b6672f0cc32a0
Pedro <salazar9_5@hotmail.com>
2025-09-26 08:53:10 -0700

    devoliciones

--- Biblioteca/Program.cs
+++ Biblioteca/Program.cs
@@ -13,83 +13,119 @@ namespace Biblioteca
             List<Libro> libros = RepositorioLibros.ObtenerLibros();
             List<Categoria> categorias = RepositorioCategorias.ObtenerCategorias();
             List<Cliente> clientes = RepositorioClientes.ObtenerClientes();
+            //Console.WriteLine("Categorias");
+            //foreach (Categoria categoria in categorias)
+            //{
+            //    Console.WriteLine(categoria.Nombre);
+            //}
 
-            Console.WriteLine("Libros");
-            foreach (Libro libro in libros)
-            {
-                Console.WriteLine($"{libro.ID} - {libro.Titulo}");
-            }
-
-            Console.WriteLine("Categorias");
-            foreach (Categoria categoria in categorias)
-            {
-                Console.WriteLine(categoria.Nombre);
-            }
-
-            Console.WriteLine("Clientes");
-            foreach (Cliente cliente in clientes)
-            {
-                Console.WriteLine($"{cliente.ID} - {cliente.Nombre}");
-            }
 
             List<Prestamo> prestamos = new List<Prestamo>();
             Prestamo prestamo = new Prestamo();
-            Libro libroPrestado= null;
+            Libro libroPrestado = null;
             Cliente clienteActual = null;
-
-            Console.WriteLine("Que desea realizar:");
-            Console.WriteLine("0 - Salir");
-            Console.WriteLine("1 - Prestamo");
-            Console.WriteLine("2 - Deolucion");
-            Console.WriteLine("3 - Ver Prestamos");
-            int opcion = Convert.ToInt32(Console.ReadLine());
-
-
-            if (opcion == 1)
+            int opcion = 0;
+            do
             {
-                Console.WriteLine("Ingresar Datos de prestamo");
-                while (libroPrestado == null)
+                Console.WriteLine("");
+                Console.WriteLine("--------MENU---------");
+                Console.WriteLine("Que desea realizar:");
+                Console.WriteLine("0 - Salir");
+                Console.WriteLine("1 - Prestamo");
+                Console.WriteLine("2 - Deolucion");
+                Console.WriteLine("3 - Ver Prestamos");
+                Console.WriteLine("4 - Ver Clientes");
+                Console.WriteLine("5 - Ver Libros");
+
+                opcion = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+
+                if (opcion == 1)
                 {
-                    Console.WriteLine("Indique el libro: ");
-                    int idLibro = Convert.ToInt32(Console.ReadLine());
-                    libroPrestado = RepositorioLibros.ObtenerLibroPorId(idLibro);
-                    if (libroPrestado == null)
+                    prestamo = new Prestamo();
+                    libroPrestado = null;
+                    clienteActual = null;
+                    prestamo.ID = prestamos.Count + 1;//asignacion de prueba de id
+                    Console.WriteLine("Ingresar Datos de prestamo");
+                    while (libroPrestado == null)
                     {
-                        Console.WriteLine("El libro no existe!!!!!");
+                        Console.WriteLine("Indique el libro: ");
+                        int idLibro = Convert.ToInt32(Console.ReadLine());
+                        libroPrestado = RepositorioLibros.ObtenerLibroPorId(idLibro);
+                        if (libroPrestado == null)
+                        {
+                            Console.WriteLine("El libro no existe!!!!!");
+                        }
+                        else
+                        {
+                            prestamo.Libro = libroPrestado;
+                        }
                     }
-                    else
+                    while (clienteActual == null)
                     {
-                        prestamo.Libro = libroPrestado;
+                        Console.WriteLine("Indique el cliente: ");
+                        int idCliente = Convert.ToInt32(Console.ReadLine());
+                        clienteActual = RepositorioClientes.ObtenerClientePorId(idCliente);
+                        if (clienteActual == null)
+                        {
+                            Console.WriteLine("El cliente no existe!!!!!");
+                        }
+                        else
+                        {
+                            prestamo.Cliente = clienteActual;
+                        }
+                    }
+                    prestamo.FechaPrestamo = DateTime.Now;
+                    prestamos.Add(prestamo);
+                    Console.WriteLine("El prestamo fue realizado correctamente");
+                }
+                else if (opcion == 2)
+                {
+                    int idPrestamo = 0;
+                    Console.Write("Indique el prestamoa  devolver:");
+                    idPrestamo = Convert.ToInt32(Console.ReadLine());
+                    int _index = prestamos.FindIndex(p => p.ID == idPrestamo);
+                    prestamos[_index].FechaDev = DateTime.Now;
+                    Console.WriteLine("La devolicion fue realizada correctamente");
+
+                }
+                else if (opcion == 3)
+                {
+                    //Console.WriteLine("| ID |        Titulo        |   CLiente    | Fecha Prestamo    |    Fecha Dev  |");
+                    //Console.WriteLine("-------------------------------------------------------------------------");
+                    foreach (Prestamo p in prestamos)
+                    {
+                        DateTime fechadev = p.FechaDev;
+                        string sFechaDev = " ------ ";
+                        if (fechadev > Convert.ToDateTime("01-02-0001"))
+                        {
+                            sFechaDev= fechadev.ToString();
+                        }
+                        Console.WriteLine($"| " +
+                            $"{p.ID} | " +
+                            $"{p.Libro.Titulo} | " +
+                            $"{p.Cliente.Nombre} | " +
+                            $"{p.FechaPrestamo} | " +
+                            $"{sFechaDev} |");
                     }
                 }
-                while (clienteActual == null)
+                else if (opcion == 4)
                 {
-                    Console.WriteLine("Indique el cliente: ");
-                    int idCliente = Convert.ToInt32(Console.ReadLine());
-                    clienteActual = RepositorioClientes.ObtenerClientePorId(idCliente);
-                    if (clienteActual == null)
+                    foreach (Cliente cliente in clientes)
                     {
-                        Console.WriteLine("El cliente no existe!!!!!");
-                    }
-                    else
-                    {
-                        prestamo.Cliente = clienteActual;
+                        Console.WriteLine($"{cliente.ID} - {cliente.Nombre}");
                     }
                 }
-                prestamo.FechaPrestamo = DateTime.Now;
-                prestamos.Add(prestamo);
-            }
-            else if (opcion == 2)
-            {
+                else if (opcion == 5)
+                {
+                    foreach (Libro libro in libros)
+                    {
+                        Console.WriteLine($"{libro.ID} - {libro.Titulo}");
+                    }
+                }
+            } while (opcion > 0);
 
-            }
-            else if (opcion == 3)
-            {
-                foreach(Prestamo p in prestamos)
-                {
-                    Console.WriteLine($"{p.ID} - {p.Libro.Titulo} - {p.Cliente.Nombre}");
-                }
-            }
         }
     }
 }
